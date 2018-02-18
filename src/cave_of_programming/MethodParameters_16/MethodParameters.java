1f6781ef0aee964068d448c1621e2d428a2032a2
Gaurav Singh <gaurav.singh55@gmail.com>
2018-02-18 14:31:12 +0530

    Methods, setters and constructor examples added.

---
+++ src/cave_of_programming/MethodParameters_16/MethodParameters.java
@@ -0,0 +1,28 @@
+package cave_of_programming.MethodParameters_16;
+
+class Robot {
+    public void speak(String greeting) {
+        System.out.println(greeting);
+    }
+
+    // methods can have multiple parameters (direction, distance)
+    // Note here String is a reference type, i.e. its a label to something that gets passed around
+    // but double is a primitive which means its like a bucket with some value and the value gets
+    // passed around.
+    public void move(String direction, double distance) {
+        System.out.println("Okay, Moving in direction " + direction + " for " + distance + " meters");
+    }
+}
+
+
+public class MethodParameters {
+    public static void main(String[] args) {
+        Robot jarvis = new Robot();
+
+        String customGreeting = "Hello World, I am jarvis";
+        jarvis.speak(customGreeting);
+
+        // method call
+        jarvis.move("North", 5.98);
+    }
+}

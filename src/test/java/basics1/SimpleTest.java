639b54f07227e6d5dbebd16f57322a21086b5884
Gaurav Singh <gaurav.singh55@gmail.com>
2018-02-21 22:06:15 +0530

    Moved all files into associated package with valid name and added maven support.

---
+++ src/test/java/basics1/SimpleTest.java
@@ -0,0 +1,26 @@
+package basics1;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class SimpleTest {
+
+    @BeforeClass
+    public void setUp() {
+        System.out.println("Ran before tests");
+    }
+
+    /**
+     * "@Test" tells which functions are Test's and function names can be any
+     * test method can below to several groups.
+     */
+    @Test(groups = { "fast" })
+    public void aFastTest() {
+        System.out.println("Fast test");
+    }
+
+    @Test(groups = { "slow" })
+    public void aSlowTest() {
+        System.out.println("Slow test");
+    }
+}
